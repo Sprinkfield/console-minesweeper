978f3c8e65232710c95009c85719dd000c1e3c06
Sprinkfield <103107451+Sprinkfield@users.noreply.github.com>
2022-06-08 00:48:32 +0700

    Update MinesweeperStyle2.py

--- MinesweeperStyle2.py
+++ MinesweeperStyle2.py
@@ -41,19 +41,16 @@ def create_mined_field():
     mines_coords = []
 
     # Placement of mines
-    for _ in range(amount_of_mines):
-        def loop_placement():
+    mines_counter = amount_of_mines
 
-            mine_x = randint(1, size_of_field - 1)
-            mine_y = randint(1, size_of_field - 1)
+    while mines_counter:
+        mine_x = randint(1, size_of_field - 1)
+        mine_y = randint(1, size_of_field - 1)
 
-            if [mine_x, mine_y] not in mines_coords:
-                mined_field[mine_x][mine_y] = 1
-                mines_coords.append([mine_x, mine_y])
-            else:
-                loop_placement()
-
-        loop_placement()
+        if (mine_x, mine_y) not in mines_coords:
+            mined_field[mine_x][mine_y] = 1
+            mines_coords.append((mine_x, mine_y))
+            mines_counter -= 1
 
     for i in range(size_of_field):
         mined_field[0][i] = 2
