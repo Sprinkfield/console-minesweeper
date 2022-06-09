abf6eb8d9ebe59acd602654df0f9659454856ed9
Sprinkfield <103107451+Sprinkfield@users.noreply.github.com>
2022-06-09 18:38:58 +0700

    Update MinesweeperStyle1.py

--- MinesweeperStyle1.py
+++ MinesweeperStyle1.py
@@ -143,10 +143,9 @@ def mine_check():
     else:
         showing_field[y][x] = str(check_neighbouring_mines(x, y)) + ' '
 
-        if showing_field[y][x] in NUM_VARS:
-            if str(check_neighbouring_flags(x, y)) + ' ' == showing_field[y][x]:
-                check_neighbouring_numbers(x, y)
-                clear_field()
+        if (showing_field[y][x] in NUM_VARS) and (str(check_neighbouring_flags(x, y)) + ' ' == showing_field[y][x]):
+            check_neighbouring_numbers(x, y)
+            clear_field()
 
         if showing_field[y][x] == '0 ':
             check_neighbouring_numbers(x, y)
